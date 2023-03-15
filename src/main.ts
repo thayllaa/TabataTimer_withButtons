b2bf477d30c0876cf6b29fccc95305e3a95cc5d0
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-15 20:11:09 -0300

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TimerComponent],
   template: `
     <h1>{{name}}</h1>
+    <app-timer></app-timer>
   `,
 })
 export class App {
--- src/timer/timer.component.ts
+++ src/timer/timer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
+  standalone: true,
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
