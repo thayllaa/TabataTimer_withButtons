9f1b5c3a2cd6becbe82235b88cd494f044b39f1a
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-15 20:50:55 -0300

    1 file modified

--- src/timer/timer.component.ts
+++ src/timer/timer.component.ts
@@ -8,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   start() {}
 
